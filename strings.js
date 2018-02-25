cb63f6457a82e29725f4543650aa8b5b59c190c1
MohamadAlkhaldi <mmalkhaldi119@cit.just.edu.jo>
2018-02-25 09:39:43 +0200

    week4 day2

--- strings.js
+++ strings.js
@@ -2,13 +2,27 @@
 // 	Input :: "a"
 // 	Ouput :: "Position of alphabet: 1"
 	function characPosit(character){
-		//your code is here
+		var str = 'abcdefghijklmnopqrstuvwxyz'
+		return 'position in alphabet: ' + str.indexOf(character)
 	}
 
 // Write a function called repeatStr which repeats the given
 // string string exactly n times.
 // repeatStr("hello",4) ==> 'hellohellohellohello'
-	
+	/*
 	function repeatStr (n, s) {
 		//your code is here
+		var str = ''
+		for(var i = 0; i < s; i++){
+			str += n
+		}
+		return str
+	}
+	*/
+
+	function repeatStr (n, s){
+		if (s === 0){
+			return ''
+		}
+		return n + repeatStr2(n, s-1)
 	}
