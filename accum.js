451391ed609994c5b38ac37797a2b82059c8f405
MohamadAlkhaldi <mmalkhaldi119@cit.just.edu.jo>
2018-02-27 09:30:49 +0200

    w4 d4 toyProblem

--- accum.js
+++ accum.js
@@ -4,6 +4,21 @@
 // accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt");    // "C-Ww-Aaa-Tttt"
 
+	function repeatStr(str, num){
+		var str1 = ''
+		while(num > 0){
+			str1 += str.toLowerCase()
+			num = num - 1
+		}
+		return str1
+
+	}
+
     function accum(input){
         //your code is here
+        var arr = input.split('')
+        for(var i =0; i < arr.length; i++){
+        	arr[i] = arr[i].toUpperCase() + repeatStr(arr[i][0], i)
+        }
+        return arr.join('-')
     }
