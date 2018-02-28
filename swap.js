fa26e6ccb22d6252473ae78a80e1bc6450a272c8
MohamadAlkhaldi <mmalkhaldi119@cit.just.edu.jo>
2018-02-28 09:41:35 +0200

    w4 d5 toyProblem

--- swap.js
+++ swap.js
@@ -3,4 +3,15 @@
 	// ToYPRoblEm --> tOyprOBLeM
 	function swap(input){
 		//your code is here
+		var str = ''
+		for(var i = 0; i < input.length;i++){
+			if(input[i] === input[i].toUpperCase()){
+			str = str + input.slice(i, i+1).toLowerCase()
+		}	else if(input[i] === input[i].toLowerCase()){
+			str = str + input.slice(i, i+1).toUpperCase()
+		}
+		}
+		return str
 	}
+
+	
