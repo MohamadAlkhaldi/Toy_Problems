183f213710ff80e5d019b032980a5e35fc8ac456
MohamadAlkhaldi <mmalkhaldi119@cit.just.edu.jo>
2018-03-01 09:21:58 +0200

    Last toy problem is done

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -3,6 +3,25 @@
 	// 22 ===> 20
 	// 25 ===> 30
 	// 37 ===> 40
+ function incToTen (n)){
+ 	while(n%10 !== 0){
+ 		n++
+ 	}
+ 	return n
+ }
+
+ function decToTen (n)){
+ 	while(n%10 !== 0){
+ 		n--
+ 	}
+ 	return n
+ }
+
  function closestMultipleOf10(num) {
  	//your code is here
+ 	if(incToTen(num)-num > num-decToTen(num)){
+ 		return decToTen(num)
+ 	}
+ 	return incToTen(num)
+
  }
